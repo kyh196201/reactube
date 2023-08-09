9d749c4f078e47e4b55494a4128c06d51075928f
seungwoo Kim <kyh196201@gmail.com>
2023-08-10 00:59:39 +0900

    feat: Home 페이지 로컬 json 데이터 적용

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -2,58 +2,7 @@ import { Link } from 'react-router-dom';
 import VideoChannel from './VideoChannel';
 
 export default function VideoItem(props) {
-  const { video: _video } = props;
-
-  const video = _video || {
-    id: '4fikvcuirtY',
-    thumbnail: 'https://i.ytimg.com/vi/4fikvcuirtY/hqdefault.jpg',
-    categoryId: '24',
-    publishedAt: '2022-09-24T19:59:59Z',
-    title: 'BLACKPINK(블랙핑크) - Shut Down @인기가요 inkigayo 20220925',
-    description:
-      'BLACKPINK - Shut Down #SBSInkigayo_EP1155\n블랙핑크 - Shut Down #BLACKPINK #ShutDown\n\nSBS Inkigayo(인기가요) is a Korean music program broadcast by SBS. \n\nThe show features some of K-pop artists’ performance every Sunday.\nCheck out this week’s Inkigayo Line up and meet your favorite artist!\n🔗bit.ly/3epKoUp',
-    tags: [
-      'Inkigayo',
-      '인기가요직캠',
-      '안방1열직캠',
-      '스브스케이팝',
-      '스브스 직캠',
-      '인기가요',
-      '인가',
-      '음방',
-      '음악방송',
-      '블랙핑크',
-      'BLACKPINK',
-      'Shut Down',
-      '블랙핑크 컴백',
-      '블랙핑크 정규',
-      '블랙핑크 인기가요',
-      '블랙핑크 인가',
-      '블랙핑크 무대',
-      '블랙핑크 안무',
-      '블랙핑크 직캠',
-      '블랙핑크 페이스캠',
-      '블랙핑크 풀캠',
-      '블랙핑크 신곡',
-      '핑크베놈',
-      '셧다운',
-      '지수',
-      '제니',
-      '로제',
-      '리사',
-      'JISOO',
-      'JENNIE',
-      'ROSÉ',
-      'LISA',
-    ],
-    channel: {
-      id: 'UCFL1sCAksD6_7JIZwwHcwjQ',
-      title: 'JTBC Entertainment',
-      customUrl: '@jtbcentertainment',
-      thumbnail:
-        'https://yt3.ggpht.com/Xfjhq84VhhTQ2UDHPoHKypmbm-3qA5J3Oo2Qvg6dmIOdDOlznkuBA28fvT_aLVv9vXZh5TMIwQ=s800-c-k-c0x00ffffff-no-rj',
-    },
-  };
+  const { video } = props;
 
   return (
     <div>
@@ -70,12 +19,12 @@ export default function VideoItem(props) {
 
       {/* 비디오 정보 */}
       <div className="flex">
-        <VideoChannel channel={video.channel} />
+        {video.channel && <VideoChannel channel={video.channel} />}
 
         <Link to="video" className="pl-3 pr-6">
           <h3 className="line-clamp-2 text-custom-black">{video.title}</h3>
           <strong className="text-custom-gray text-sm font-normal">
-            {video.channel.title}
+            {video.channel && video.channel.title}
           </strong>
           <div className="flex items-center text-custom-gray text-sm">
             <span>조회수 525회</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,142 +1,52 @@
+import { useEffect, useState } from 'react';
+import VideoItem from '../components/VideoItem';
+
 export default function Home() {
+  const [videos, setVideos] = useState([]);
+
+  // 비디오 목록 조회
+  async function fetchPopularVideos() {
+    const response = await fetch('videos/popular.json');
+    const { items } = await response.json();
+
+    const videoItems = items.map(video => {
+      const { id, snippet } = video;
+
+      const thumbnail =
+        snippet.thumbnails.high?.url ??
+        snippet.thumbnails.medium?.url ??
+        snippet.thumbnails.default?.url;
+
+      return {
+        id,
+        thumbnail,
+        categoryId: snippet.categoryId,
+        publishedAt: snippet.publishedAt,
+        title: snippet.title,
+        description: snippet.description,
+        tags: snippet.tags,
+        channel: null,
+      };
+    });
+
+    setVideos(videoItems);
+  }
+
+  useEffect(() => {
+    fetchPopularVideos();
+  }, []);
+
   return (
     <div>
       <h2 className="sr-only">홈 페이지</h2>
 
       <section className="my-8 mx-4">
-        <ul className="grid grid-cols-3 gap-4">
-          <li>
-            <a href=".">
-              {/* 비디오 컴포넌트 */}
-              <div>
-                {/* 비디오 썸네일 */}
-                <figure className="aspect-video mb-3 overflow-hidden rounded-xl">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="https://i.ytimg.com/vi/udM2SlggK9g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIFMoTzAP&rs=AOn4CLAcaAeZNppK7WLbcmb6mXevM4KkMg"
-                    alt="더 퍼스트 슬램덩크 트레일러 영상 모음 - ost 第ゼロ感"
-                  />
-                </figure>
-
-                {/* 비디오 정보 */}
-                <div className="flex">
-                  <figure className="shrink-0 w-9 h-9 mr-3">
-                    <img
-                      className="w-full h-full object-cover"
-                      src="https://yt3.ggpht.com/lh7GiITaDKNdh6hi2GasqJrfi6AqDaj0qqR1UvVWGZPjltJmYzuftmA65KDy_Tltu6S8k82WkQ=s88-c-k-c0x00ffffff-no-rj"
-                      alt="10쇄를 찍자_편집자 일기"
-                    />
-                  </figure>
-
-                  <div className="mr-6">
-                    <h3 className="line-clamp-2 text-custom-black">
-                      &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음 - ost
-                      第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음
-                      - ost 第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러
-                      영상 모음 - ost 第ゼロ感
-                    </h3>
-                    <strong className="text-custom-gray text-sm font-normal">
-                      10쇄를 찍자_편집자 일기
-                    </strong>
-                    <div className="flex items-center text-custom-gray text-sm">
-                      <span>조회수 525회</span>
-                      <span className="before:content-['•'] before:mx-1">
-                        12시간 전
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href=".">
-              {/* 비디오 컴포넌트 */}
-              <div>
-                {/* 비디오 썸네일 */}
-                <figure className="aspect-video mb-3 overflow-hidden rounded-xl">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="https://i.ytimg.com/vi/udM2SlggK9g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIFMoTzAP&rs=AOn4CLAcaAeZNppK7WLbcmb6mXevM4KkMg"
-                    alt="더 퍼스트 슬램덩크 트레일러 영상 모음 - ost 第ゼロ感"
-                  />
-                </figure>
-
-                {/* 비디오 정보 */}
-                <div className="flex">
-                  <figure className="shrink-0 w-9 h-9 mr-3">
-                    <img
-                      className="w-full h-full object-cover"
-                      src="https://yt3.ggpht.com/lh7GiITaDKNdh6hi2GasqJrfi6AqDaj0qqR1UvVWGZPjltJmYzuftmA65KDy_Tltu6S8k82WkQ=s88-c-k-c0x00ffffff-no-rj"
-                      alt="10쇄를 찍자_편집자 일기"
-                    />
-                  </figure>
-
-                  <div className="mr-6">
-                    <h3 className="line-clamp-2 text-custom-black">
-                      &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음 - ost
-                      第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음
-                      - ost 第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러
-                      영상 모음 - ost 第ゼロ感
-                    </h3>
-                    <strong className="text-custom-gray text-sm font-normal">
-                      10쇄를 찍자_편집자 일기
-                    </strong>
-                    <div className="flex items-center text-custom-gray text-sm">
-                      <span>조회수 525회</span>
-                      <span className="before:content-['•'] before:mx-1">
-                        12시간 전
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href=".">
-              {/* 비디오 컴포넌트 */}
-              <div>
-                {/* 비디오 썸네일 */}
-                <figure className="aspect-video mb-3 overflow-hidden rounded-xl">
-                  <img
-                    className="w-full h-full object-cover"
-                    src="https://i.ytimg.com/vi/udM2SlggK9g/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIFMoTzAP&rs=AOn4CLAcaAeZNppK7WLbcmb6mXevM4KkMg"
-                    alt="더 퍼스트 슬램덩크 트레일러 영상 모음 - ost 第ゼロ感"
-                  />
-                </figure>
-
-                {/* 비디오 정보 */}
-                <div className="flex">
-                  <figure className="shrink-0 w-9 h-9 mr-3">
-                    <img
-                      className="w-full h-full object-cover"
-                      src="https://yt3.ggpht.com/lh7GiITaDKNdh6hi2GasqJrfi6AqDaj0qqR1UvVWGZPjltJmYzuftmA65KDy_Tltu6S8k82WkQ=s88-c-k-c0x00ffffff-no-rj"
-                      alt="10쇄를 찍자_편집자 일기"
-                    />
-                  </figure>
-
-                  <div className="mr-6">
-                    <h3 className="line-clamp-2 text-custom-black">
-                      &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음 - ost
-                      第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러 영상 모음
-                      - ost 第ゼロ感 &apos;더 퍼스트 슬램덩크&apos; 트레일러
-                      영상 모음 - ost 第ゼロ感
-                    </h3>
-                    <strong className="text-custom-gray text-sm font-normal">
-                      10쇄를 찍자_편집자 일기
-                    </strong>
-                    <div className="flex items-center text-custom-gray text-sm">
-                      <span>조회수 525회</span>
-                      <span className="before:content-['•'] before:mx-1">
-                        12시간 전
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
+        <ul className="grid grid-cols-3 gap-x-4 gap-y-8">
+          {videos.map(video => (
+            <li key={video.id}>
+              <VideoItem video={video} />
+            </li>
+          ))}
         </ul>
       </section>
     </div>
