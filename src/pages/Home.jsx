8d28ef3b6b149f05f99555c22f3227e563ea2089
seungwoo Kim <kyh196201@gmail.com>
2023-08-13 23:12:27 +0900

    feat: Home 페이지 채널 로컬 json 데이터 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import VideoItem from '../components/VideoItem';
 
 export default function Home() {
   const [videos, setVideos] = useState([]);
+  const [channels, setChannels] = useState([]);
 
   // 비디오 목록 조회
   async function fetchPopularVideos() {
@@ -24,17 +25,64 @@ export default function Home() {
         publishedAt: snippet.publishedAt,
         title: snippet.title,
         description: snippet.description,
-        tags: snippet.tags,
-        channel: null,
+        tags: snippet.tags ?? [],
+        channel: {
+          id: snippet.channelId,
+          title: snippet.channelTitle,
+        },
       };
     });
 
-    setVideos(videoItems);
+    setVideos(prev => {
+      return [...prev, ...videoItems];
+    });
+
+    return videoItems ?? [];
   }
 
-  useEffect(() => {
-    fetchPopularVideos();
+  // 채널 목록 조회
+  // eslint-disable-next-line no-unused-vars
+  async function fetchChannels(channelIds = []) {
+    const response = await fetch('videos/channel.json');
+    const { items } = await response.json();
+
+    const channelItems = items.map(channel => {
+      const { id, snippet } = channel;
+
+      const thumbnail =
+        snippet.thumbnails.high?.url ??
+        snippet.thumbnails.medium?.url ??
+        snippet.thumbnails.default?.url;
+
+      return {
+        id,
+        thumbnail,
+        title: snippet.title,
+        description: snippet.description,
+      };
+    });
+
+    setChannels(prev => {
+      return [...prev, ...channelItems];
+    });
+  }
+
+  const fetchVideosAndChannels = useCallback(async () => {
+    const videoList = await fetchPopularVideos();
+    const channelIds = videoList.map(video => video.channel.id);
+
+    await fetchChannels(channelIds);
   }, []);
+
+  useEffect(() => {
+    fetchVideosAndChannels();
+  }, [fetchVideosAndChannels]);
+
+  function getChannelInfoById(channelId) {
+    const matched = channels.find(channel => channel.id === channelId);
+
+    return matched ?? null;
+  }
 
   return (
     <div>
@@ -44,7 +92,10 @@ export default function Home() {
         <ul className="grid grid-cols-3 gap-x-4 gap-y-8">
           {videos.map(video => (
             <li key={video.id}>
-              <VideoItem video={video} />
+              <VideoItem
+                video={video}
+                channel={getChannelInfoById(video.channel.id)}
+              />
             </li>
           ))}
         </ul>
