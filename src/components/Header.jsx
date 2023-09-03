41e98393f99ab43127b9156b76092ba827429f45
seungwoo Kim <kyh196201@gmail.com>
2023-09-03 23:18:19 +0900

    feat: SearchBar 컴포넌트 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import SearchBar from './SearchBar';
 
 export default function Header() {
   return (
@@ -12,19 +13,7 @@ export default function Header() {
       </h1>
 
       <div className="flex flex-1 justify-center">
-        {/* header search component */}
-        <div className="flex w-1/2 items-center">
-          <input
-            type="text"
-            className="flex-1 h-10 border-2 border-gray-300 text-base rounded-l-full pl-4 pr-1 focus:border-blue-500 transition-colors duration-100"
-          />
-          <button
-            type="button"
-            className="h-10 border-2 border-l-0 border-gray-300 rounded-r-full w-16 bg-gray-50 hover:bg-gray-100 transition-colors duration-100"
-          >
-            검색
-          </button>
-        </div>
+        <SearchBar />
       </div>
     </header>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import {
+  useNavigate,
+  useSearchParams,
+  createSearchParams,
+} from 'react-router-dom';
+
+export default function SearchBar() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    // TODO: hook으로 분리
+    const query = searchParams.get('query');
+
+    if (query) {
+      setInputValue(prev => {
+        if (prev !== query) {
+          return query;
+        }
+
+        return prev;
+      });
+    }
+  }, [searchParams]);
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const query = inputValue.trim();
+
+    if (!query) {
+      return;
+    }
+
+    navigate({
+      pathname: '/search',
+      search: `?${createSearchParams({ query })}`,
+    });
+  }
+
+  return (
+    <form className="flex w-1/2 items-center" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="flex-1 h-10 border-2 border-gray-300 text-base rounded-l-full pl-4 pr-1 focus:border-blue-500 transition-colors duration-100"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <button
+        type="submit"
+        className="flex justify-center items-center h-10 border-2 border-l-0 border-gray-300 rounded-r-full w-16 bg-gray-50 hover:bg-gray-100 transition-colors duration-100"
+      >
+        <BsSearch />
+      </button>
+    </form>
+  );
+}
